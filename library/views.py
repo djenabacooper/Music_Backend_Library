a0271e33a331580fdc528e981f0a7876474361dd
Djenaba <djenabacooper@gmail.com>
2023-05-01 11:08:56 -0500

    Updated project urls + created view function + created library app urls file

--- library/views.py
+++ library/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
-
+from rest_framework.decorators import api_view
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Song
+from .serializers import SongSerializer
 # Create your views here.
+
+
+@api_view(['GET', 'POST'])
+def library_list(request):
+
+    if request.method == 'GET':
+        songs = Song.objects.all()
+        serializer = SongSerializer(songs, many=True)
+        return Response(serializer.data)
+    
+
+#    elif request.method == 'POST':
+#        serializer = SongSerializer(data=request.data)
+#        serializer.is_valid(raise_exception=True)
+#        serializer.save()
+#        return Response(serializer.data, status=status.HTTP_201_CREATED)
+#       
+#@api_view(['GET', 'PUT', 'DELETE'])
+#def song_detail(request, pk):
+#    
+#    if request.method == 'Get':
+#        song = get_object_or_404(Product, pk=pk)
+#        serializer = SongSerializer(product);
+#        return Response(serializer.data)
+#    elif request.method == 'PUT':
+#        song = get_object_or_404(Product, pk=pk)
+#        serializer = SongSerializer(song, data=request.data)
+#        serializer.is_valid(raise_exception=True)
+#        serializer.save()
+#        return Response(serializer.data)
+#    elif request.method == 'DELETE':
+#        song = get_object_or_404(Song, pk=pk)
+#        song.delete()
+#        return Response(status=status.HTTP_204_NO_CONTENT)
